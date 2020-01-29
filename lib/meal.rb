0728eb567b78c3684943e8e2e215d76cc23d11da
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-29 04:57:22 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,5 +1,18 @@
-def meal
-    Meal.all.select do |meal|
-      meal.customer == self
-    end
+class Meal
+ 
+  attr_accessor :waiter, :customer, :total, :tip
+ 
+  @@all = []
+ 
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
 end
