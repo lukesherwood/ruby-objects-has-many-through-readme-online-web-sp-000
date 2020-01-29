f17950f61f4a0d7b72bdae9de8d13baca19d329e
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-29 04:56:00 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,14 @@
 class Customer
-  
-end
+  attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
